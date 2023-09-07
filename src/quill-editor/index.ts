7004cf53ff3be50250354bf0693f6b5317ca7211
Paulo Wells <paulowells@gmail.com>
2023-09-07 17:13:14 -0300

    fixed exports

--- src/quill-editor/index.ts
+++ src/quill-editor/index.ts
@@ -1,14 +1,13 @@
-import Quill, { QuillOptionsStatic } from "quill";
-
-export type QuillTypes = {
-    quill: Quill;
-    quillOptions: QuillOptionsStatic
-};
-
-export { Provider } from "./Provider";
-export { Editor } from "./Editor";
-export { useEditor, useEditorHeight } from "./hooks";
-
 import "quill/dist/quill.bubble.css";
 import "quill/dist/quill.snow.css";
 import "./index.css";
+
+import Quill, { QuillOptionsStatic } from "quill";
+
+export type QuillType = Quill;
+export type QuillOptions = QuillOptionsStatic;
+export type { Config } from "./Provider/Provider";
+
+export { Provider } from "./Provider";
+export { Editor } from "./Editor";
+export { useEditor } from "./hooks";
