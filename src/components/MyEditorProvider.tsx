8388ec4cf07c192415316dc3975c05c5ac5ae7bc
Paulo Wells <paulowells@gmail.com>
2023-09-07 17:15:16 -0300

    rewrites

--- src/components/MyEditorProvider.tsx
+++ src/components/MyEditorProvider.tsx
@@ -1,22 +1,25 @@
 import { FC, ReactNode } from "react";
 import { Provider } from "react-quill-editor";
+import type { Config } from "react-quill-editor";
 import { customFonts } from "../config/myCustomFonts";
 
 interface MyEditorProps {
     children: ReactNode;
 }
 
+const config: Config = {
+    options: {
+        theme: "snow",
+        modules: {
+            toolbar: "#toolbar"
+        },
+    },
+    fonts: customFonts
+};
+
 const MyEditorProvider: FC<MyEditorProps> = ({ children }) => {
     return (
-        <Provider config={{ 
-            options: {
-                theme: "snow", 
-                modules: {
-                    toolbar: "#toolbar"
-                },
-            }, 
-            fonts: customFonts 
-            }}>
+        <Provider config={config}>
             {children}
         </Provider>
     );
